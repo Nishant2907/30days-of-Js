2f07e9f2135ec8e947f45fb82e905e280d12577d
Nishant Mishra <nishdrummer@gmail.com>
2024-07-14 21:46:08 +0530

    feat: Added Day 2 feature tasks

---
+++ Day-2/feature-request/index.js
@@ -0,0 +1,33 @@
+// Day 2
+
+
+// Feature 1: Arithmetic Operations Script
+let num1 = 10
+let num2 = 20
+
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 / num2)
+console.log(num1 % num2)
+
+// Feature 2: Comparison and Logical Operators Script
+let num3 = 30
+let num4 = 40
+
+console.log(num3 == num4);
+console.log(num3 != num4);
+console.log(num3 > num4);
+console.log(num3 < num4);
+console.log(num3 >= num4);
+console.log(num3 <= num4);
+console.log(num3 === num4);
+console.log(num3 !== num4);
+console.log(num3 > num4 && num3 < num4);
+console.log(num3 > num4 || num3 < num4);
+
+
+// Feature 3: Ternary Operator Script
+let num5 = 50
+let isPositive = num21 > 0 ? 'Positive' : 'Negative'
+console.log(isPositive);
