f31e4e957011b6bdd444958e0a6d5784c1a793b5
Nishant Mishra <nishdrummer@gmail.com>
2024-07-14 21:27:30 +0530

    feat: Added Day 2 tasks

---
+++ Day-2/task/index.js
@@ -0,0 +1,81 @@
+// Day 2
+
+// Activity 1: Arithmetic Operations
+// Task 1: Add two numbers
+let num1 = 10;
+let num2 = 20;
+let sum = num1 + num2;
+console.log(sum);
+
+// Task 2: Subtract two numbers
+let num3 = 30;
+let num4 = 40;
+let difference = num3 - num4;
+console.log(difference);
+
+// Task 3: Multiply two numbers
+let num5 = 50;
+let num6 = 60;
+let product = num5 * num6;
+console.log(product);
+
+// Task 4: Divide two numbers
+let num7 = 70;
+let num8 = 80;
+let quotient = num7 / num8;
+console.log(quotient);
+
+// Task 5: Find the remainder
+let num9 = 90;
+let num10 = 100;
+let remainder = num9 % num10;
+console.log(remainder);
+
+// Activity 2: Assignment Operators
+// Task 6: Use += operator
+let num11 = 110;
+num11 += 120;
+console.log(num11);
+
+// Task 7: Use -= operator
+let num12 = 140;
+num11 -= 130;
+console.log(num11);
+
+// Activity 3: Comparison Operators
+// Task 8: Compare using > and <
+let num13 = 150;
+let num14 = 160;
+console.log(num13 > num14);
+console.log(num13 < num14);
+
+// Task 9: Compare using >= and <=
+let num15 = 170
+let num16 = 180
+console.log(num15 >= num16);
+console.log(num15 <= num16);
+
+// Task 10: Compare using == and ===
+let num17 = '190'
+let num18 = 190
+console.log(num17 >= num18);
+console.log(num17 <= num18);
+
+// Activity 4: Logical Operators
+// Task 11: Use && operator
+let num19 = true
+let num20 = false
+console.log(num19 && num20);
+
+// Task 12: Use || operator
+console.log(num19 || num20);
+
+// Task 13: Use ! operator
+console.log(!num19);
+
+// Activity 5: Ternary Operator
+// Task 14: Check if number is positive or negative
+let num21 = 200
+let isPositive = num21 > 0 ? 'Positive' : 'Negative'
+console.log(isPositive);
+
